b6cac121bd1174af2857769edd05e35673f9c4dd
kizitoomoit <kizitoomoit9@gmail.com>
2019-01-15 15:09:07 +0300

    This is a simple program used to add two numbers and it is written in C++.

---
+++ addtwo.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	int b;
+	int c;
+
+	cout << "Enter the first number: ";
+	cin >> a;
+	cout << endl;
+
+	cout << "Enter the second number: ";
+	cin >> b;
+	cout << endl;
+
+	c = a + b;
+
+	cout << "The answer after addition := " << c << endl;
+
+	return 0;
+
+}
